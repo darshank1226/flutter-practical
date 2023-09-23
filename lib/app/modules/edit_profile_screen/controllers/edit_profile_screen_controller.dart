4d220d758400f4d15994288f4fb731fb14b93b72
darshank1226 <kachhadiyadarshan90@gmail.com>
2023-09-23 11:30:08 +0530

    add image picker functionality

--- lib/app/modules/edit_profile_screen/controllers/edit_profile_screen_controller.dart
+++ lib/app/modules/edit_profile_screen/controllers/edit_profile_screen_controller.dart
@@ -1,9 +1,28 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 
 class EditProfileScreenController extends GetxController {
   TextEditingController nameController = TextEditingController();
   TextEditingController emailController = TextEditingController();
   TextEditingController skillsController = TextEditingController();
   TextEditingController workExperienceController = TextEditingController();
+
+  Rx<File?> avatarImage = Rx<File?>(null);
+
+  void setAvatarImage(File image) {
+    avatarImage.value = image;
+  }
+
+  Future<void> pickupImage() async {
+    final XFile? pickedFile =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      // Handle the picked image (you can set it to the CircleAvatar or save it as needed)
+      // For example, you can set it as the CircleAvatar background:
+      setAvatarImage(File(pickedFile.path));
+    }
+  }
 }
--- lib/app/modules/edit_profile_screen/views/edit_profile_screen_view.dart
+++ lib/app/modules/edit_profile_screen/views/edit_profile_screen_view.dart
@@ -12,100 +12,138 @@ class EditProfileScreenView extends GetView<EditProfileScreenController> {
   const EditProfileScreenView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Edit Profile'),
-        centerTitle: true,
-      ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Stack(
+    return WillPopScope(
+        onWillPop: () {
+          return _showExitConfirmationDialog(context);
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Edit Profile'),
+            centerTitle: true,
+          ),
+          body: SingleChildScrollView(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                CircleAvatar(
-                  radius: 80,
-                  backgroundImage: NetworkImage(
-                      'https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80'),
-                ),
-                Positioned(
-                  bottom: 0,
-                  right: 0,
-                  child: Container(
-                    decoration: BoxDecoration(
-                        color: Colors.white.withOpacity(0.7),
-                        shape: BoxShape.circle,
-                        border:
-                            Border.all(color: AppColor.primaryColor, width: 2)),
-                    child: IconButton(
-                      icon: Icon(Icons.edit),
-                      onPressed: () {},
+                Stack(
+                  children: <Widget>[
+                    CircleAvatar(
+                      radius: 80,
+                      backgroundImage: controller.avatarImage.value != null
+                          ? Image.file(controller.avatarImage.value!).image
+                          : NetworkImage(
+                              'https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80'),
                     ),
-                  ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white.withOpacity(0.7),
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                                color: AppColor.primaryColor, width: 2)),
+                        child: IconButton(
+                          icon: Icon(Icons.edit),
+                          onPressed: () {
+                            controller.pickupImage();
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
+                SizedBox(height: 20),
+                TextFormField(
+                  controller: controller.nameController,
+                  decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: AppColor.primaryColor, width: 2)),
+                      labelStyle: TextStyle(color: AppColor.primaryColor),
+                      border: OutlineInputBorder(),
+                      labelText: 'Name'),
+                ),
+                SizedBox(height: 10),
+                TextFormField(
+                  controller: controller.emailController,
+                  decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: AppColor.primaryColor, width: 2)),
+                      labelStyle: TextStyle(color: AppColor.primaryColor),
+                      border: OutlineInputBorder(),
+                      labelText: 'Email'),
+                ),
+                SizedBox(height: 10),
+                TextFormField(
+                  controller: controller.skillsController,
+                  decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: AppColor.primaryColor, width: 2)),
+                      labelStyle: TextStyle(color: AppColor.primaryColor),
+                      border: OutlineInputBorder(),
+                      labelText: 'Skills'),
+                ),
+                SizedBox(height: 10),
+                TextFormField(
+                  controller: controller.workExperienceController,
+                  decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              color: AppColor.primaryColor, width: 2)),
+                      labelStyle: TextStyle(color: AppColor.primaryColor),
+                      border: OutlineInputBorder(),
+                      labelText: 'Work Experience'),
+                ),
+                SizedBox(height: 20),
+                CommonElevatedButton(
+                    buttonText: "Save",
+                    fontSize: 16,
+                    borderRadius: 0,
+                    backgroundColor: AppColor.primaryColor,
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('Profile Updated Successfully'),
+                        ),
+                      );
+                    })
               ],
             ),
-            SizedBox(height: 20),
-            TextFormField(
-              controller: controller.nameController,
-              decoration: InputDecoration(
-                  focusedBorder: OutlineInputBorder(
-                      borderSide:
-                          BorderSide(color: AppColor.primaryColor, width: 2)),
-                  labelStyle: TextStyle(color: AppColor.primaryColor),
-                  border: OutlineInputBorder(),
-                  labelText: 'Name'),
+          ),
+        ));
+  }
+
+  Future<bool> _showExitConfirmationDialog(BuildContext context) async {
+    final result = await showDialog<bool>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Discard Changes?'),
+          content:
+              Text('Do you want to leave this page without saving changes?'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(true); // Return true to allow leaving
+              },
+              child: Text('Yes'),
             ),
-            SizedBox(height: 10),
-            TextFormField(
-              controller: controller.emailController,
-              decoration: InputDecoration(
-                  focusedBorder: OutlineInputBorder(
-                      borderSide:
-                          BorderSide(color: AppColor.primaryColor, width: 2)),
-                  labelStyle: TextStyle(color: AppColor.primaryColor),
-                  border: OutlineInputBorder(),
-                  labelText: 'Email'),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context)
+                    .pop(false); // Return false to stay on the page
+              },
+              child: Text('No'),
             ),
-            SizedBox(height: 10),
-            TextFormField(
-              controller: controller.skillsController,
-              decoration: InputDecoration(
-                  focusedBorder: OutlineInputBorder(
-                      borderSide:
-                          BorderSide(color: AppColor.primaryColor, width: 2)),
-                  labelStyle: TextStyle(color: AppColor.primaryColor),
-                  border: OutlineInputBorder(),
-                  labelText: 'Skills'),
-            ),
-            SizedBox(height: 10),
-            TextFormField(
-              controller: controller.workExperienceController,
-              decoration: InputDecoration(
-                  focusedBorder: OutlineInputBorder(
-                      borderSide:
-                          BorderSide(color: AppColor.primaryColor, width: 2)),
-                  labelStyle: TextStyle(color: AppColor.primaryColor),
-                  border: OutlineInputBorder(),
-                  labelText: 'Work Experience'),
-            ),
-            SizedBox(height: 20),
-            CommonElevatedButton(
-                buttonText: "Save",
-                fontSize: 16,
-                borderRadius: 0,
-                backgroundColor: AppColor.primaryColor,
-                onPressed: () {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text('Profile Updated Successfully'),
-                    ),
-                  );
-                })
           ],
-        ),
-      ),
+        );
+      },
     );
+
+    return result ?? false; // Return false if result is null
   }
 }
