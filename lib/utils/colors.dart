d0a534151e8192b759ba25a1f410910469ba1e51
darshank1226 <kachhadiyadarshan90@gmail.com>
2023-09-24 10:56:14 +0530

    remove common color from utils

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -3,8 +3,5 @@ import 'package:flutter/material.dart';
 class AppColor {
   static const Color primaryColor = Color(0XFF000000);
   static const Color whiteColor = Color(0XFFFFFFFF);
-  static const Color greenColor = Color(0xFF4CAF50);
-  static const Color redColor = Color(0xFFF44336);
   static const Color greyColor = Color(0xFF757575);
-  static const Color blueColor = Color(0xFF0707c9);
 }
